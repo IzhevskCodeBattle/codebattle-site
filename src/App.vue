34615a28d4d4d742e975018e18ba8ed4bb02ae05
Denis Pankratov <denis_pankratov@epam.com>
2018-05-09 23:27:03 +0400

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,54 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
-  </div>
+ <div id="app">
+   <v-app dark>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer class="blue darken-2">
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3">
+            Made with
+            <v-icon class="red--text">favorite</v-icon>
+            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+            and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+ </div>
 </template>
 
+
 <script>
+
+// <template>
+//  <div id="app">
+//    <header>
+//      <span>Vue.js PWA</span>
+//    </header>
+//    <main>
+//      <img src="./assets/logo.png" alt="Vue.js PWA">
+//      <router-view></router-view>
+//    </main>
+//  </div>
+// </template>
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      title: 'Your Logo'
+    }
+  }
 }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-</style>
