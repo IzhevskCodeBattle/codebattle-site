429fe04a40e3d00477e4f5c84aed6466c07d53ab
katyunya <ekaterinammail@gmail.com>
2018-06-29 16:36:31 +0400

    feature/ui-login added styles to social icons

--- src/view/Auth/Signin.vue
+++ src/view/Auth/Signin.vue
@@ -18,34 +18,33 @@
               </v-flex>
             </v-layout>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn type="submit" color="blue darken-1" flat>Login</v-btn>
+          <v-btn outline @click.native="dialog = false">Close</v-btn>
+          <v-btn outline type="submit">Login</v-btn>
         </v-card-actions>
         <p class="message text-xs-center">
           Not registered?
-          <v-btn color="blue darken-1" flat @click.native="login=false">Create an account</v-btn>
+          <v-btn flat @click.native="login=false">Create an account</v-btn>
         </p>
         <v-card-title class="justify-center">
           <span class="headline">Войти через:</span>
         </v-card-title>
-        <v-card-text class="text-xs-center">
-          <a href="http://212.158.174.142:4333/api/auth/vkontakte"><v-icon>fab fa-vk</v-icon></a>
-          <a href=""><v-icon>fab fa-facebook</v-icon></a>
-          <a href=""><v-icon>fab fa-twitter</v-icon></a>
-          <a href=""><v-icon>fab fa-linkedin-in</v-icon></a>
-          <a href=""><v-icon>fab fa-github</v-icon></a>
-          <a href=""><v-icon>fab fa-google</v-icon></a>
-                </v-card-text>
+        <v-card-text style="display:flex; justify-content:center; padding-bottom: 16px">
+          <a class="vk" href="http://212.158.174.142:4333/api/auth/vkontakte"><v-icon>fab fa-vk</v-icon></a>
+          <a class="fb" href=""><v-icon>fab fa-facebook-f</v-icon></a>
+          <a class="twitter" href=""><v-icon>fab fa-twitter</v-icon></a>
+          <a class="linkedin" href=""><v-icon>fab fa-linkedin-in</v-icon></a>
+          <a class="github" href=""><v-icon>fab fa-github-alt</v-icon></a>
+          <a class="google" href=""><v-icon>fab fa-google</v-icon></a>
+        </v-card-text>
       </v-card>
     </form>
   </v-dialog>
 
   <v-dialog v-if="!login" v-model="dialog" persistent max-width="500px">
-    <v-btn slot="activator" color="primary">signin</v-btn>
+    <v-btn flat slot="activator">Signin</v-btn>
     <form class="register-form" @submit.prevent="signup()">
       <v-card max-width="500px">
         <v-card-title class="justify-center">
@@ -65,19 +64,16 @@
               </v-flex>
             </v-layout>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false; login=true">Close</v-btn>
-          <v-btn type="submit" color="blue darken-1" flat>Signup</v-btn>
+          <v-btn outline @click.native="dialog = false; login=true">Close</v-btn>
+          <v-btn outline type="submit">Signup</v-btn>
         </v-card-actions>
-        <v-card-text class="text-xs-center">
-          <p class="message">
+           <p class="message text-xs-center">
             Already registered?
-            <v-btn color="blue darken-1" flat @click.native="login=true">Signin</v-btn>
+            <v-btn flat @click.native="login=true">Signin</v-btn>
           </p>
-        </v-card-text>
       </v-card>
     </form>
   </v-dialog>
@@ -157,14 +153,83 @@ export default {
 <style scoped>
 .login-form,
 .register-form {
-  font-family: 'Source Sans Pro';
+  font-family: "Source Sans Pro";
+}
+
+span {
+  color: #464547;
+}
+
+.card__text {
+  padding: 0px 16px 0px 16px;
 }
 
 a {
   text-decoration: none;
+  display: flex;
+  width: 50px;
+  min-width: 50px;
+  height: 50px;
+  background: lightgrey;
+  border-radius: 50%;
+  margin: 3px;
+  justify-content: center;
+}
+
+p {
+  margin-bottom: 0px;
+}
+
+.vk:hover {
+  background: #597da3;
+}
+
+.fb:hover {
+  background: #3b5998;
+}
+
+.twitter:hover {
+  background: #00aced;
+}
+
+.linkedin:hover {
+  background: #007bb6;
+}
+
+.github:hover {
+  background: black;
+}
+
+.google:hover {
+  background: #dd4b39;
 }
 
 .icon {
-  font-size: 50px;
+  font-size: 40px;
+  color: white;
+}
+
+.btn {
+  color: #263852;
+}
+
+
+@media only screen and (max-width: 370px) {
+  a {
+    text-decoration: none;
+    display: flex;
+    width: 40px;
+    min-width: 40px;
+    height: 40px;
+    background: lightgrey;
+    border-radius: 50%;
+    margin: 3px;
+    justify-content: center;
+  }
+
+  .icon {
+    font-size: 30px;
+    color: white;
+  }
 }
 </style>
