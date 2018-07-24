69231fd8221ecf0c6a8aa65ccdd62c776a6eb39b
NikaClemente <nikaclemente007@gmail.com>
2018-07-24 10:49:36 +0400

    Events 2

--- src/component/Events.vue
+++ src/component/Events.vue
@@ -4,29 +4,21 @@
     <v-layout column>
       <h2 class="toolbar">Events</h2>
       <v-layout row wrap>
-        <v-flex class='xs12 sm6 md4 lg4' v-for="card in cards" :key="card.title">
+        <v-flex class='xs12 sm6 md4 lg4' v-for="event in events" :key="event.name">
           <v-card>
-            <div >
-              <img :src="card.src"/>
-            </div>
-            <div>
-              <v-card-title primary-title class="headline cards">{{card.title}}</v-card-title>
-              <v-card-text class="title_all">
-                {{card.title_all}}
-              </v-card-text>
-            </div>
-            <!-- <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn icon> 
-                <v-icon>favorite</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>bookmark</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>share</v-icon>
-              </v-btn>
-            </v-card-actions> -->
+            <v-card-media height="250px" :src="event.src">
+              <v-container fill-height>
+                <v-layout>
+                  <v-flex class='xs12 sm12 md12 lg12'>
+                    <span class="headline cards">{{event.name}}</span>
+                    <span class="cards">{{event.autor}}</span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-media>
+            <v-card-text class="title_all">
+              {{event.title_all}}
+            </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
@@ -34,33 +26,40 @@
   </section>
 </template>
 
+
 <!-- код, который относится непосредственно к компоненту -->
 <script>
 export default {
   data: () => ({
-    cards: [
-      { title: 'Название игры №1',
+    events: [
+      { name: 'Название игры №1',
         src: '/static/img/games/games1.jpg',
+        autor: 'Автор: Имя фамилия',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
       },
-      { title: 'Название игры №2',
+      { name: 'Название игры №2',
         src: '/static/img/games/games2.jpg',
+        autor: 'Автор: Имя фамилия',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
       },
-      { title: 'Название игры №3',
+      { name: 'Название игры №3',
         src: '/static/img/games/games1.jpg',
+        autor: 'Автор: Имя фамилия',
+        title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
+      },
+      { name: 'Название игры №4',
+        src: '/static/img/games/games2.jpg',
+        autor: 'Автор: Имя фамилия',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
       }
-      // { title: 'Название игры №4',
-      //   src: '/static/img/games/games2.jpg',
-      //   title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
-      // },
-      // { title: 'Название игры №5',
+      // { name: 'Название игры №5',
       //   src: '/static/img/games/games1.jpg',
+      //   autor: 'Автор: Имя фамилия',
       //   title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
       // },
-      // { title: 'Название игры №6',
+      // { name: 'Название игры №6',
       //   src: '/static/img/games/games2.jpg',
+      //   autor: 'Автор: Имя фамилия',
       //   title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...'
       // }
     ]
@@ -86,11 +85,16 @@ export default {
     padding-bottom: 15px;
   }
   .card{
-    position: relative;
-    overflow: hidden;
+    /* position: relative; */
+    /* overflow: hidden; */
     margin: 2px;
   }  
+  .cards{
+    /* position: relative; */
+    text-align: center;
+  }
   .title_all{
+    position: relative;
     text-align: justify;
     font-size: 1.3em;
   }
