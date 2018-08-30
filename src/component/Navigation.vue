482839e12b2cdd80f1c04e2b900a213b33dfc9b7
Ekaterina Smolnikova <Ekaterina_Smolnikova@epam.com>
2018-08-30 12:31:14 +0400

    small fixes

--- src/component/Navigation.vue
+++ src/component/Navigation.vue
@@ -4,28 +4,28 @@
     <v-btn flat class="scrollactive-item" to="/home#about">
       <v-list-tile-avatar>
         <v-icon>far fa-question-circle</v-icon>
-      </v-list-tile-avatar>About</v-btn>
+      </v-list-tile-avatar>О ПРОЕКТЕ</v-btn>
     <v-btn flat class="scrollactive-item" to="/home#events">
       <v-list-tile-avatar>
         <v-icon>far fa-calendar-alt</v-icon>
-      </v-list-tile-avatar>Events</v-btn>
+      </v-list-tile-avatar>СОБЫТИЯ</v-btn>
     <v-btn flat class="scrollactive-item" to="/home#games">
       <v-list-tile-avatar>
         <v-icon>fas fa-gamepad</v-icon>
-      </v-list-tile-avatar>Games</v-btn>
+      </v-list-tile-avatar>ИГРЫ</v-btn>
     <v-btn flat class="scrollactive-item" to="/home#partners">
       <v-list-tile-avatar>
         <v-icon>fas fa-handshake</v-icon>
-      </v-list-tile-avatar>Partners</v-btn>
+      </v-list-tile-avatar>НАШИ ПАРТНЕРЫ</v-btn>
     <v-divider></v-divider>
     <v-btn flat @click="$events.$emit('login-form:show')">
       <v-list-tile-avatar>
         <v-icon>fas fa-sign-in-alt</v-icon>
-      </v-list-tile-avatar>Sign in</v-btn>
+      </v-list-tile-avatar>ВОЙТИ</v-btn>
     <v-btn flat @click="$events.$emit('signup-form:show')" v-if="!this.$store.getters.authenticated">
       <v-list-tile-avatar>
         <v-icon>fas fa-user-plus</v-icon>
-      </v-list-tile-avatar>Sign up</v-btn>
+      </v-list-tile-avatar>РЕГИСТРАЦИЯ</v-btn>
     <v-btn flat v-if="this.$store.getters.authenticated">
       <v-list-tile-avatar>
         <v-icon>fas fa-sign-out-alt</v-icon>
