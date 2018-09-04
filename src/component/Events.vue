3ed87aa16ff2ad44b83559cccc1016c20f923af3
Marsel Khisamov <Marsel_Khisamov@epam.com>
2018-09-04 15:47:13 +0400

    fix

--- src/component/Events.vue
+++ src/component/Events.vue
@@ -80,8 +80,6 @@ export default {
     background: url("../../static/img/done.svg");
     background-size: cover;
     background-repeat: no-repeat;
-    opacity: 2;
-
   }
   .event:hover{
     box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
