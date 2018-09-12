f38ee1833b51cf3788163fe1d0ee1750562f5ecf
Aleksandra Zaitceva <Aleksandra_Zaitceva@epam.com>
2018-09-12 13:06:49 +0400

    favicon was changed

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,15 @@ Vue.use(VueRouter)
 export default new VueRouter({
   routes,
   mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    if (to.hash) {
-      return { selector: to.hash }
-    }
+  scrollBehavior: (to, from, savedPosition) => {
     if (savedPosition) {
       return savedPosition
+    } else if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    } else {
+      return { x: 0, y: 0 }
     }
-    return false
   }
 })
