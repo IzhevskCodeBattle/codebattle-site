f6c876c05bb28b1253508678cc45c88bfcb7c637
Denis Pankratov <Denis_Pankratov@epam.com>
2018-09-12 13:37:50 +0400

    Comming Soon page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,15 @@
-import Home from '@/view/Home'
+// import Home from '@/view/Home'
 import Game from '@/view/Game'
 import Event from '@/view/Event'
 import Account from '@/view/Account'
 import Main from '@/view/Main'
+import CommingSoon from '@/view/CommingSoon'
 
 export default [
   { path: '/',
     component: Main,
     children: [
-      { path: '', name: 'home', component: Home },
+      { path: '', name: 'home', component: CommingSoon },
       { path: 'game/:id', name: 'game', component: Game },
       { path: 'event/:id', name: 'event', component: Event },
       { path: 'user/:id', name: 'user', component: Account }
