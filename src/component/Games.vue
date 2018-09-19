4ea47de2f0c99455745a5265a4f0302ca36fdd5b
Denis Pankratov <Denis_Pankratov@epam.com>
2018-09-19 22:02:02 +0400

    a few corrections

--- src/component/Games.vue
+++ src/component/Games.vue
@@ -34,21 +34,21 @@ export default {
         title: 'LodeRunner',
         src: '/static/img/games/loderunner-min.jpg',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
-        status: 'В разработке...',
+        status: 'Игра готова',
         isComplite: true
       },
       { id: 2,
         title: 'Battle City',
         src: '/static/img/games/battlecity.jpg',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
-        status: 'В разработке...',
+        status: 'Игра готова',
         isComplite: true
       },
       { id: 4,
         title: 'Лунолёт',
         src: '/static/img/games/lunolet.jpg',
         title_all: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
-        status: 'В разработке...',
+        status: 'Игра готова',
         isComplite: true
       }
     ]
