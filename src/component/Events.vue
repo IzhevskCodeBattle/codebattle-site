bcad428becef6bce5a5bae19313380567e0f791e
Marsel Khisamov <Marsel_Khisamov@epam.com>
2018-10-01 16:28:25 +0400

    stage1

--- src/component/Events.vue
+++ src/component/Events.vue
@@ -181,8 +181,8 @@ export default {
     box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
   }
   .past-event__background {
-    max-width: 180px;
-    max-height: 180px;
+    max-width: 120px;
+    max-height: 120px;
     object-fit: cover;
     opacity: 0.4;
   }
